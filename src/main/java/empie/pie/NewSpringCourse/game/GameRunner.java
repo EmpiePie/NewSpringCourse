f0767ba80eb67df91baedd9adb0c78ca38a1a5d2
EmpiePie <raoul.schwenke@gmail.com>
2022-03-28 21:36:46 +0200

    Update

--- src/main/java/empie/pie/NewSpringCourse/game/GameRunner.java
+++ src/main/java/empie/pie/NewSpringCourse/game/GameRunner.java
@@ -7,11 +7,20 @@ import org.springframework.stereotype.Component;
 public class GameRunner {
 
     @Autowired
+    //Field Injection. Or Constructor Injection if Constructor is active below.
+    //Constructor Injection Recommended
     private GamingConsole game;
 
     public GameRunner(GamingConsole game) {
+    System.out.println("Using Constructor");
         this.game = game;
     }
+
+//    @Autowired
+//    public void setGame(GamingConsole game) {
+//        System.out.println("Using Setter");
+//        this.game = game;
+//    }
 
     public void runGame(){
         game.up();
--- src/main/java/empie/pie/NewSpringCourse/game/SuperContraGame.java
+++ src/main/java/empie/pie/NewSpringCourse/game/SuperContraGame.java
@@ -1,5 +1,8 @@
 package empie.pie.NewSpringCourse.game;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SuperContraGame implements GamingConsole {
 
     public void up() {
